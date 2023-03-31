bad4c90f052855ec99740deec86f6a92ee0042b5
Soldy <4786022+Soldy@users.noreply.github.com>
2023-03-31 12:34:38 +0100

    test timeout extension
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.cpp
+++ test.cpp
@@ -9,11 +9,11 @@ TEST_SUITE("simple test") {
     TEST_CASE("init" * doctest::timeout(0.00003)) {
         simple_test = new cpp_sssvg::Svg();
     };
-    TEST_CASE("startTag" * doctest::timeout(0.000025)){
+    TEST_CASE("startTag" * doctest::timeout(0.00003)){
         std::string result = cpp_sssvg::startTag("svg");
         CHECK(result == "<svg>");
     };
-    TEST_CASE("startTag with attributes" * doctest::timeout(0.000025)){
+    TEST_CASE("startTag with attributes" * doctest::timeout(0.00003)){
         std::string result = cpp_sssvg::startTag("svg", {{"width","400"},{"height","500"}});
         CHECK(result == "<svg height=\"500\" width=\"400\">");
     };
