d40a4bea1d81041616bb3d7b5652a6fc0e519e87
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-21 13:38:42 +0000

    polyline test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test_polyline.cpp
@@ -0,0 +1,32 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <string>
+#include "doctest.h"
+#include "polyline.hpp"
+
+
+
+
+std::string polyline;
+TEST_SUITE("polyline test") {
+    TEST_CASE("Creation" * doctest::timeout(0.00009)) {
+        polyline = cpp_sssvg::polyline(
+          cpp_sssvg::points(
+           {
+             {20,20},
+             {40,200},
+             {60,40},
+             {80,180},
+             {120,60},
+             {140,160}
+           }
+          ),
+          cpp_sssvg::style(
+            "#000000",
+            3
+          )
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(polyline == "<polyline points=\"20,20 40,200 60,40 80,180 120,60 140,160\" style=\"stroke:#000000;stroke-width:3;fill:none\" />");
+    };
+};
