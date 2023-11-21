25d5aab53deaebcb476df1cb5d90332745d29322
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-21 12:44:43 +0000

    ellipse test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test_ellipse.cpp
@@ -0,0 +1,24 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <string>
+#include "doctest.h"
+#include "ellipse.hpp"
+
+
+std::string ellipse;
+TEST_SUITE("ellipse test") {
+    TEST_CASE("Creation" * doctest::timeout(0.00009)) {
+        ellipse = cpp_sssvg::ellipse(
+          10,
+          10,
+          60,
+          30,
+          cpp_sssvg::style(
+            "#000000",
+            3
+          )
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(ellipse == "<ellipse cx=\"10\" cy=\"10\" rx=\"60\" ry=\"30\" style=\"stroke:#000000;stroke-width:3;fill:none\" />");
+    };
+};
