259735e8468d2af6b60e16864c285517a820e87a
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-21 12:28:00 +0000

    rectangle implementation
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test_rectangle.cpp
@@ -0,0 +1,26 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <string>
+#include "doctest.h"
+#include "rectangle.hpp"
+
+
+std::string rectangle;
+TEST_SUITE("rectangle test") {
+    TEST_CASE("Creation" * doctest::timeout(0.00009)) {
+        rectangle = cpp_sssvg::rectangle(
+          10,
+          10,
+          10,
+          10,
+          0,
+          0,
+          cpp_sssvg::style(
+            "#000000",
+            3
+          )
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(rectangle == "<rect cx=\"10\" cy=\"10\" height=\"10\" rx=\"0\" ry=\"0\" style=\"stroke:#000000;stroke-width:3;fill:none\" width=\"10\" />");
+    };
+};
