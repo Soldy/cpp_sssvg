5670a8e025aa434de53342a9e7e5524088db3f54
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-24 07:16:54 +0000

    test extension
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test/test_style.cpp
+++ test/test_style.cpp
@@ -4,15 +4,38 @@
 #include "../include/style.hpp"
 
 
-std::string style;
+std::string style_2;
+std::string style_3;
+std::string style_4;
 TEST_SUITE("style test") {
     TEST_CASE("create style" * doctest::timeout(0.00003)) {
-        style = cpp_sssvg::style(
+        style_2 = cpp_sssvg::style(
           "#000000",
           3
         );
     };
     TEST_CASE("Correct value"){
-        CHECK(style == "stroke:#000000;stroke-width:3;fill:none");
+        CHECK(style_2 == "stroke:#000000;stroke-width:3;fill:none");
+    };
+    TEST_CASE("create style" * doctest::timeout(0.00003)) {
+        style_3 = cpp_sssvg::style(
+          "#000000",
+          3,
+          "#00ff00"
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(style_3 == "stroke:#000000;stroke-width:3;fill:#00ff00");
+    };
+    TEST_CASE("create style" * doctest::timeout(0.00003)) {
+        style_4 = cpp_sssvg::style(
+          "#000000",
+          3,
+          "#00ff00",
+          "evenodd"
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(style_4 == "stroke:#000000;stroke-width:3;fill:#00ff00;fill-rule:evenodd");
     };
 };
