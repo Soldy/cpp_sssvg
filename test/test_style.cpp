b6a070fccfce82db835367df45a44367a7ba963e
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-24 07:40:58 +0000

    style test extension with map
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test/test_style.cpp
+++ test/test_style.cpp
@@ -1,5 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <string>
+#include <vector>
+#include <array>
+#include <unordered_map>
 #include "../doctest.h"
 #include "../include/style.hpp"
 
@@ -7,8 +10,9 @@
 std::string style_2;
 std::string style_3;
 std::string style_4;
+std::string style_vector;
 TEST_SUITE("style test") {
-    TEST_CASE("create style" * doctest::timeout(0.00003)) {
+    TEST_CASE("create style 2" * doctest::timeout(0.00003)) {
         style_2 = cpp_sssvg::style(
           "#000000",
           3
@@ -24,7 +28,7 @@ TEST_SUITE("style test") {
           "#00ff00"
         );
     };
-    TEST_CASE("Correct value"){
+    TEST_CASE("Correct value 3"){
         CHECK(style_3 == "stroke:#000000;stroke-width:3;fill:#00ff00");
     };
     TEST_CASE("create style" * doctest::timeout(0.00003)) {
@@ -35,7 +39,20 @@ TEST_SUITE("style test") {
           "evenodd"
         );
     };
-    TEST_CASE("Correct value"){
+    TEST_CASE("Correct value 4"){
         CHECK(style_4 == "stroke:#000000;stroke-width:3;fill:#00ff00;fill-rule:evenodd");
+    };
+    TEST_CASE("Create vector style" * doctest::timeout(0.00003)) {
+        style_4 = cpp_sssvg::style(
+          {
+            {"stroke", "#000000"},
+            {"stroke-width", "3"},
+            {"fill", "#00ff00"},
+            {"fill-rule", "evenodd"}
+          }
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(style_4 == "fill:#00ff00;fill-rule:evenodd;stroke:#000000;stroke-width:3");
     };
 };
