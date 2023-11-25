1301e397661af1efa2ec98226490d8e19c2b7365
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-25 10:45:43 +0000

    text test extension
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test/test_text.cpp
+++ test/test_text.cpp
@@ -5,6 +5,7 @@
 
 
 std::string text;
+std::string text_style;
 TEST_SUITE("text test") {
     TEST_CASE("Creation" * doctest::timeout(0.00009)) {
         text = cpp_sssvg::text(
@@ -15,5 +16,19 @@ TEST_SUITE("text test") {
     };
     TEST_CASE("Correct value"){
         CHECK(text == "<text x=\"10\" y=\"10\">test</text>");
+    };
+    TEST_CASE("Creation with style" * doctest::timeout(0.00009)) {
+        text_style = cpp_sssvg::text(
+          10,
+          10,
+          cpp_sssvg::style(
+            "#000000",
+            3
+          ),
+          "test"
+        );
+    };
+    TEST_CASE("Correct value"){
+        CHECK(text_style == "<text style=\"stroke:#000000;stroke-width:3;fill:none\" x=\"10\" y=\"10\">test</text>");
     };
 };
