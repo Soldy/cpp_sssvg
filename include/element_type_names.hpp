dd65e972f7cf9bcb31beb970c4bca109603dca46
Soldy <4786022+Soldy@users.noreply.github.com>
2023-11-26 18:40:39 +0000

    element type names
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ include/element_type_names.hpp
@@ -0,0 +1,77 @@
+#ifndef __CPP_SSSVG_ELEMENT_TYPE_NAMES_HPP_
+#define __CPP_SSSVG_ELEMENT_TYPE_NAMES_HPP_
+#include <vector>
+#include <string>
+
+
+namespace cpp_sssvg{
+
+std::vector<
+  std::string
+> element_type_names {
+    "a",
+    "animate",
+    "animateMotion",
+    "circle",
+    "clipPath",
+    "defs",
+    "desc",
+    "ellipse",
+    "feBlend",
+    "feColorMatrix",
+    "feComponentTransfer",
+    "feComposite",
+    "feConvolveMatrix",
+    "feDiffuseLighting",
+    "feDisplacementMap",
+    "feDistanfLight",
+    "feDropShadow",
+    "feFlood",
+    "feFuncA",
+    "feFuncB",
+    "feFuncG",
+    "feFuncR",
+    "feGaussianBlur",
+    "feImage",
+    "feMerge",
+    "feMergeNode",
+    "feMorphology",
+    "feOffset",
+    "fePointLight",
+    "feSpecularLighting",
+    "feSpotLight",
+    "feTile",
+    "feTurbulence",
+    "filter",
+    "g",
+    "image",
+    "line",
+    "linearGradient",
+    "marker",
+    "mask",
+    "metadata",
+    "mpath",
+    "path",
+    "pattern",
+    "polygon",
+    "polyline",
+    "radialGradient",
+    "rect",
+    "script",
+    "set",
+    "stop",
+    "style",
+    "svg",
+    "switch",
+    "symbol",
+    "text",
+    "textPath",
+    "title",
+    "tspan",
+    "use",
+    "view"
+};
+
+}
+
+#endif
